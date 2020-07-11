f0343c808b6c0ca7c63167496fa014b0731c0bce
Ricardo Mendez <ricardomendezvillalpando@gmail.com>
2020-07-10 20:01:27 -0500

    heroku version

---
+++ server/build/precache-manifest.728b460b0ee5846c39b48c6718ba2eb7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3629d8ffbe4d71f62b892a53327da3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afbdb4f8402dd3260398",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8c319c2542d75e2e2a36",
+    "url": "/static/css/main.cefb35ea.chunk.css"
+  },
+  {
+    "revision": "afbdb4f8402dd3260398",
+    "url": "/static/js/2.10d5d3ad.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.10d5d3ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c319c2542d75e2e2a36",
+    "url": "/static/js/main.453c5133.chunk.js"
+  },
+  {
+    "revision": "5cea1fe8e46a75505742",
+    "url": "/static/js/runtime-main.60d46b0a.js"
+  },
+  {
+    "revision": "e292d61b8d306fd1ddc8dabe7e521266",
+    "url": "/static/media/moons.e292d61b.svg"
+  }
+]);
